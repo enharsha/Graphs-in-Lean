e1acb0ef5c25fda1376bf6c8c54aa58a2e0873bd
enharsha <56439762+enharsha@users.noreply.github.com>
2019-12-12 23:40:05 +0530

    Update graph.lean

--- graph.lean
+++ graph.lean
@@ -9,18 +9,18 @@ structure graph  :=
 #print graph
 
 /-graph with single point and loop-/
-inductive ele:Type:=
-|0
-def a (x:ele):ele:= ele.rec_on x 0
+inductive ele:Type
+|p1:ele
+def a (x:ele):ele:= ele.rec_on x ele.p1
 def graph0:graph := {vertex:=ele,edge:=ele,φ := prod.mk a a}
 #print graph0
 
 /-graph with 2 points and an edge-/
-inductive ele_2:Type:=
-|0
-|1
-def b1 (x:ele):ele_2:=ele.rec_on x 0
-def b2 (x:ele):ele_2:=ele.rec_on x 1
+inductive ele_2:Type
+|p1:ele_2
+|p2:ele_2
+def b1 (x:ele):ele_2:=ele.rec_on x ele_2.p1
+def b2 (x:ele):ele_2:=ele.rec_on x ele_2.p2
 def graph1:graph := {vertex:=ele_2,edge:=ele,φ := prod.mk b1 b2}
 #print graph1
 
