f2ad9a926e68a924b6d8147ac1f8796028954345
enharsha <56439762+enharsha@users.noreply.github.com>
2019-12-18 01:49:10 +0530

    Update graph.lean

--- graph.lean
+++ graph.lean
@@ -25,8 +25,14 @@ def b2 (x:ele):ele_2:=ele.rec_on x ele_2.p2
 def graph1:graph := {vertex:=ele_2,edge:=ele,φ1 :=b1,φ2:= b2}
 #print graph1
 
-inductive path (g:graph)(start:g.vertex):(g.vertex)→ Type u
+inductive path (g:graph)(start:g.vertex):(g.vertex)→ Type u:=
 |fix : path start 
 |addedge (add:g.edge)(end1:g.vertex)(p: path end1)(pr:end1 = g.φ1 add): path (g.φ2 add) 
 
+open path
 #check path 
+constant path1: path graph0 ele.p1 ele.p1  
+#check path1
+#print path1
+
+constant path2 : path graph1 ele_2.p1 ele_2.p1 
