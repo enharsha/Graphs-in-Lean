a0d9bbce2d4d805cf82149ceaf5e04e2e0f1f5ad
enharsha <56439762+enharsha@users.noreply.github.com>
2019-12-17 22:45:42 +0530

    Update graph.lean

--- graph.lean
+++ graph.lean
@@ -3,7 +3,8 @@ variable α :Type u
 structure graph  :=
 (vertex : Type u)
 (edge : Type v)
-(φ : (edge→ vertex)× (edge→ vertex))
+(φ1 : (edge→ vertex))
+(φ2 :(edge→ vertex))
 
 #check graph
 #print graph
@@ -12,7 +13,7 @@ structure graph  :=
 inductive ele:Type
 |p1:ele
 def a (x:ele):ele:= ele.rec_on x ele.p1
-def graph0:graph := {vertex:=ele,edge:=ele,φ := prod.mk a a}
+def graph0:graph := {vertex:=ele,edge:=ele,φ1 := a,φ2:=a}
 #print graph0
 
 /-graph with 2 points and an edge-/
@@ -21,6 +22,6 @@ inductive ele_2:Type
 |p2:ele_2
 def b1 (x:ele):ele_2:=ele.rec_on x ele_2.p1
 def b2 (x:ele):ele_2:=ele.rec_on x ele_2.p2
-def graph1:graph := {vertex:=ele_2,edge:=ele,φ := prod.mk b1 b2}
+def graph1:graph := {vertex:=ele_2,edge:=ele,φ1 :=b1,φ2:= b2}
 #print graph1
 
