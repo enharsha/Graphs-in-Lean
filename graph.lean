d33e4fa7504109f200dc162d73fd033d8604de48
enharsha <56439762+enharsha@users.noreply.github.com>
2019-12-20 09:54:49 +0530

    Update graph.lean

--- graph.lean
+++ graph.lean
@@ -10,29 +10,29 @@ structure graph  :=
 #print graph
 
 /-graph with single point and loop-/
-inductive ele:Type
-|p1:ele
-def a (x:ele):ele:= ele.rec_on x ele.p1
-def graph0:graph := {vertex:=ele,edge:=ele,φ1 := a,φ2:=a}
+inductive One:Type
+|one:One
+def a (x:One):One:= One.one
+def graph0:graph := {vertex:=One,edge:=One,φ1 := a,φ2:=a}
 #print graph0
 
 /-graph with 2 points and an edge-/
-inductive ele_2:Type
-|p1:ele_2
-|p2:ele_2
-def b1 (x:ele):ele_2:=ele.rec_on x ele_2.p1
-def b2 (x:ele):ele_2:=ele.rec_on x ele_2.p2
-def graph1:graph := {vertex:=ele_2,edge:=ele,φ1 :=b1,φ2:= b2}
+inductive Two:Type
+|one:Two
+|two:Two
+def b1 (x:One):Two:=Two.one
+def b2 (x:One):Two:=Two.two
+def graph1:graph := {vertex:=Two,edge:=One,φ1 :=b1,φ2:= b2}
 #print graph1
 
-inductive path (g:graph)(start:g.vertex):(g.vertex)→ Type u:=
+inductive path (g:graph)(start:g.vertex):(g.vertex) → Type u
 |fix : path start 
-|addedge (add:g.edge)(end1:g.vertex)(p: path end1)(pr:end1 = g.φ1 add): path (g.φ2 add) 
+|addedge (add:g.edge)(last:g.vertex)(p: path last)(pr:last = g.φ1 add): path (g.φ2 add) 
 
 open path
 #check path 
-constant path1: path graph0 ele.p1 ele.p1  
+constant path1: path graph0 One.one One.one  
 #check path1
 #print path1
 
-constant path2 : path graph1 ele_2.p1 ele_2.p1 
+  
